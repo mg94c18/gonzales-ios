46e616af3a03b8a824c4499319173239d4aa6052
Saša Stevanović <sstevano@amazon.com>
2025-03-17 03:21:20 -0700

    Ćirilica za španski, i update za najnovije prevode sa Androida.

--- druzinaOdVjesala/AssetsDV.swift
+++ druzinaOdVjesala/AssetsDV.swift
@@ -10,11 +10,18 @@ import Foundation
 
 class Assets {
     // cat ../Gonzales/app/src/gonzales/assets/numbers | sed -r 's/$/"/' | sed -r 's/^/"/' | tr -d '\r' | tr '\n' ','
-    public static let numbers: [String] = ["valemucho","interesas","manuela","nada","elrey","llorona","recordaras","libroviejo","quizas","baila2","tienesabor","basurita","sumujer2","aveces2","besame","perdi","cielorojo2","hastasiempre","cuerpo","hermoso","recuerdos2","hey2","chatarra","lutte","carretera","querria","hubierasabido","seranovia","unomas","volvera","volverb","encuestaamor","shakira"]
+    public static let numbers: [String] = ["valemucho","escucha","interesas","manuela","nada","elrey","llorona","recordaras","libroviejo","quizas","baila2","tienesabor","basurita","sumujer2","aveces2","besame","perdi","tumedas","hastasiempre","cuerpo","recuerdos2","hey2","fuiste","chatarra","querria","hubierasabido","seranovia","unomas","volver","macario","sondeoa","shakira","futuros","nosdieron","sondeob","pluma","calle","sondeov","nodal","universitarios","sondeog"]
 
-    public static let titles: [String] = ["Para siempre","Ya no me interesas","Manuela","No me importa nada","El rey","La llorona","Un ano de amor","Pedacito de papel","Quizás, quizás, quizás","Baila morena","Tiene sabor","La basurita","Quién es ese hombre","A veces tú, a veces yo","Besame mucho","Estos celos","Cielo rojo","Hasta siempre, comandante","Tu sangra en mi cuerpo","Hermoso cariño","Entre mis recuerdos","Hey","Chatarra","Lutte","Carretera","Querria","Si hubiera sabido ayer","Los Serrano 1","Uno más uno son siete","Volver 1","Volver 2","Encuesta - Bilbao","Entrevista - Las mujeres ya no lloran"]
+    public static let titles: [String] = ["Para siempre","Escúchame","Tú a mi ya no me interesas","Manuela","No me importa nada","El rey","La llorona","Un año de amor","Pedacito de papel","Quizás, quizás, quizás","Baila morena","Tiene sabor","La basurita","Quién es ese hombre","A veces tú, a veces yo","Besame mucho","Estos celos","Tan sólo tú","Hasta siempre, comandante","Tu sangra en mi cuerpo","Entre mis recuerdos","Ese siempre fui yo","Eras, pero ahora no eres","Chatarra","Querria","Si hubiera sabido ayer","Los Serrano 1","Uno más uno son siete","Su fantasma o su espíritu","Un guajolote para el solo","Guanajuato - 1","Las mujeres ya no lloran","De primaria - futuros","Y nos dieron las diez","Guanajuato - 2","Entrevista - Pluma","De primaria - calle","Guanajuato - 3","Entrevista - Nodal","De primaria - universitarios","Guanajuato - final"]
     
-    public static let dates: [String] = ["Vicente Fernández","Lucha Villa","Julio Iglesias","Luz Casal","Vicente Fernández","Chavela Vargas","Luz Casal","Buena Vista Social Club","Trio Los Panchos","Julio Iglesias","Omara Portuondo","Flor Silvestre","Zharick León","La Apuesta","Consuelo Velázquez","Vicente Fernández","Flor Silvestre","Carlos Puebla y sus Tradicionales","Ana Bertha Castellanos, Jesús Castellanos","Vicente Fernández","Luz Casal","Julio Iglesias","Waor, El Jincho, Brawler","Ayax y Prok, Fernandocosta","Natos, Waor, Recycled J","El Kanka","Joan Sebastian","Jorge Jurado, Antonio Resines, Víctor Elías, Fran Perea, Belén Rueda","Fran Perea","Carmen Maura, Lola Dueñas, Penélope Cruz, Leandro Rivera","Leandro Rivera, Penélope Cruz, Yohana Cobo, Lola Dueñas","Encuesta sobre el amor 2.0","Shakira, Lili Estefan"]
+    public static let dates: [String] = ["Vicente Fernández","Aguilas de America","Lucha Villa","Julio Iglesias","Luz Casal","Vicente Fernández","Chavela Vargas","Luz Casal","Buena Vista Social Club","Trio Los Panchos","Julio Iglesias","Omara Portuondo","Flor Silvestre","Zharick León","La Apuesta","Consuelo Velázquez","Vicente Fernández","Franco De Vita, Alejandra Guzmán","Carlos Puebla y sus Tradicionales","Ana Bertha Castellanos, Jesús Castellanos","Luz Casal","Julio Iglesias","Gilda","Waor, El Jincho, Brawler","El Kanka","Joan Sebastian","Jorge Jurado, Antonio Resines, Víctor Elías, Fran Perea, Belén Rueda","Fran Perea","Carmen Maura, Lola Dueñas, Penélope Cruz, Leandro Rivera, Yohana Cobo","Pina Pellicer, Ignacio López Tarso, José Gálvez, José Luis Jiménez, Enrique Lucero","Tv Guanajuato Canal 8","Shakira, Lili Estefan","Charlyokei","Joaqin Sabina","Tv Guanajuato Canal 8","Clarissa Molina, Peso Pluma","Charlyokei","Tv Guanajuato Canal 8","Raúl de Molina, Christian Nodal","Charlyokei","Tv Guanajuato Canal 8"]
+
+    private static let CYRILLIC_MODE = "cyrillic_mode"
+    private static var inCyrillic = UserDefaults.standard.bool(forKey: CYRILLIC_MODE)
+    public static func toggleCyrillic() {
+        inCyrillic = !inCyrillic
+        UserDefaults.standard.set(inCyrillic, forKey: CYRILLIC_MODE)
+    }
 
     public static let defaultEpisodeId: Int = 0
 
@@ -42,8 +49,9 @@ class Assets {
         let bucketSuffix = sectionInfo[index.0].2
         var ret: [String] = []
 
-        // for f in $(cat ../Gonzales/app/src/gonzales/assets/numbers); do for p in "" ".bukvalno" ".finalno"; do cp ../Gonzales/app/src/gonzales/assets/${f}${p} druzinaOdVjesala/Tekstovi/${f}${p}.txt; done; done
-        if let path = Bundle.main.path(forResource: number + withTranslation, ofType: "txt") {
+        // for f in $(cat ../Gonzales/app/src/gonzales/assets/numbers); do for p in "" ".bukvalno" ".finalno" ".bukvalno.cirilica" ".finalno.cirilica"; do cp ../Gonzales/app/src/gonzales/assets/${f}${p} druzinaOdVjesala/Tekstovi/${f}${p}.txt; done; done
+        let suffix = !withTranslation.isEmpty && inCyrillic ? withTranslation + ".cirilica" : withTranslation
+        if let path = Bundle.main.path(forResource: number + suffix, ofType: "txt") {
             do {
                 let data = try String(contentsOfFile: path, encoding: .utf8)
                 ret += data.components(separatedBy: .newlines)
